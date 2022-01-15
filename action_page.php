c4e252f31092e633bfa0c0c3cc1ec3a35628dc40
vottyn <97806105+vottyn@users.noreply.github.com>
2022-01-15 20:59:03 +0100

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="about" content="information about vottyn and it creators to help you understand it better">
+    <meta name="author" content="">
+
+    <title>error</title>
+      <link rel="icon" type="image/x-icon" href="pics/vlogo.ico">
+  </head>
+
+  <body>
+
+    <p>Invalid username</p>
+  </body>
+
+</html>
